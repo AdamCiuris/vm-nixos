008035d8a2af6635a9819418d64b1e78aa0af9c3
Adam Ciuris <adamciuris@gmail.com>
2024-04-30 17:56:06 -0500

    rdp user, plasma-manager config

---
+++ home-manager/rdpHome.nix
@@ -0,0 +1,55 @@
+{ inputs, outputs, lib, config, pkgs, ... }:
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+	home ={
+		username = "rdp";
+		homeDirectory = "/home/rdp";
+		# You should not change this value, even if you update Home Manager. If you do
+		# want to update the value, then make sure to first check the Home Manager
+		# release notes.
+		stateVersion = "23.11";
+		
+	};
+
+	# Home Manager is pretty good at managing dotfiles. The primary way to manage
+	# plain files is through 'home.file'.
+	home.file = {
+	};
+
+	home.sessionVariables = {
+		EDITOR = "nano";
+	};
+
+	# if not nixOS chsh to /usr/bin/zsh else change users.defaultShell
+		nixpkgs.config.allowUnfree=true;
+		fonts.fontconfig.enable = true;
+		# BEGIN SHELL CONFIGS
+		# BEGIN BASH
+		home.packages = with pkgs; [
+			libreoffice
+			htop
+			git
+			gimp
+			vlc
+			zsh
+			xdg-utils
+			brave
+
+			# x2g0
+			(pkgs.nerdfonts.override { fonts=["DroidSansMono" ]; }) # for vscode
+			];
+
+
+
+  imports = [
+		./configs/git.nix
+		./configs/xdg.nix
+		./configs/vscodium.nix
+		./configs/shells.nix
+		./configs/plasma.nix
+		./configs/msmtp.nix
+  ];
+
+
+}
